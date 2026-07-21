e8fd1773f8669de6dfe51605d462845012f89bae
Vaishnavi R Kannur <kannurvaishnavi@gmail.com>
2026-07-21 14:55:29 +0530

    Time: 0 ms (100.00%) | Memory: 33 MB (57.24%) - LeetSync

---
+++ 3194-find-words-containing-character/find-words-containing-character.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        int n = words.size();
+        vector<int> ans;
+        for(int i = 0; i < n; i++) {      
+            for(auto ch : words[i]) {
+            if(ch == x) {
+                ans.push_back(i);
+                break;
+            }
+        }
+        }
+            return ans;
+    }
+};
